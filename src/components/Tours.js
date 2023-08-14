2a9bde6d5e45b13a550971dc1b86db41af716e29
Aleksandr232 <aleksrowing232@gmail.com>
2023-08-15 02:25:10 +0300

    tours

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -6,27 +6,49 @@ import Menu from "./Menu";
 const Tours = () =>{
     const [tours, setTours] = useState('');
     const [username, setUsername] = useState('');
+    const [footing1_5, setFooting] = useState(''); 
+           
 
+    const handleFormReset = (e) => {
+      e.target.reset();
+    };
+
+    
     const handleSubmit = (e) => {
         e.preventDefault();
       
         axios.put('https://xn--80aagge2ckkol0hd.xn--p1ai/api/stock/1', {
           tours,
-          username
+          username,
+           footing1_5,
+          /*area0_45,
+          rama1_2,
+          rigel2,
+          link0_7,
+          rama1_1,
+          emphasis,
+          footing0_7,
+          area07_15,
+          rama0_7,
+          rigel1_5,
+          rama0_7_1,
+          footing2_4,
+          pipe,
+          rama1_4,
+          link0_9 */
+
+
         })
-          .then((response) => {
-            setTours('');
-            setUsername('');
-            console.log(response);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
+        .then((response) => {
+          handleFormReset(e);
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
       };
       
-      const handleFormReset = (e) => {
-        e.target.reset();
-      };
+      
 
     return(
         <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -37,13 +59,146 @@ const Tours = () =>{
                     <input
                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     placeholder="количиство вышки-тур"
-                    value={tours}
+                    
                     onChange={(e) => setTours(e.target.value)}
+                    />
+                     <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="основания 1,5"
+                    
+                    onChange={(e) => setFooting(e.target.value)}
+                    type="number"
+                    />
+                    {/*<input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="площадка 0,45"
+                    
+                    onChange={(e) => setArea0_45(e.target.value)}
+                    type="number"
                     />
                     <input
                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="площадка 0,45"
+                    
+                    onChange={(e) => setArea0_45(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="рама 1*2"
+                   
+                    onChange={(e) => setRama1_2(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="ригель 2м"
+                    
+                    onChange={(e) => setRigel2(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="связи 0,7"
+                    
+                    onChange={(e) => setLink0_7(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="связи 0,7"
+                    
+                    onChange={(e) => setLink0_7(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="рама 1*1 м"
+                    
+                    onChange={(e) => setRama1_1(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="упор стабильности"
+                    
+                    onChange={(e) => setEmphasis(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="основание 0,7*2"
+                    
+                    onChange={(e) => setFooting0_7(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="площадка 0,7 * 1,5 м"
+                    
+                    onChange={(e) => setArea07_15(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="рама 0,7*2 м"
+                    
+                    onChange={(e) => setRama0_7(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="рама 0,7*2 м"
+                    
+                    onChange={(e) => setRama0_7(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="ригель 1,5 м"
+                    
+                    onChange={(e) => setRigel1_5(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="рама 0,7*1 м"
+                   
+                    onChange={(e) => setRama0_7_1(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="основание 2,4 м. с 2-мя колёсами"
+                    
+                    onChange={(e) => setFooting2_4(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="соединительная профильная труба 2 м"
+                 
+                    onChange={(e) => setPipe(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="рама 1,4*2 м"
+                    
+                    onChange={(e) => setRama1_4(e.target.value)}
+                    type="number"
+                    />
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                    placeholder="связи 0,9 м"
+                    
+                    onChange={(e) => setLink0_9(e.target.value)}
+                    type="number"
+                    /> */}
+                    <input
+                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     placeholder="Имя зав склада"
-                    value={username}
+                    
                     onChange={(e) => setUsername(e.target.value)}
                     type="text"
                     />
