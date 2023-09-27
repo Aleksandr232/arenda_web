e3f95541495270b35141b16c8ad09bad00b99a3f
Aleksandr232 <aleksrowing232@gmail.com>
2023-09-28 01:34:42 +0300

    api

--- src/components/Logistica.js
+++ src/components/Logistica.js
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 
 const Logistica=()=>{
   const token = localStorage.getItem('token');
+  const [status, setStatus] = useState('данные скоро обновяться')
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(
           (position) => {
@@ -17,7 +18,7 @@ const Logistica=()=>{
       }, []);
     
       const sendLocationToAPI = (latitude, longitude) => {
-        axios.post('https://xn--80aagge2ckkol0hd.xn--p1ai/api/logist', { latitude, longitude }, {
+        axios.post('https://xn--80aagge2ckkol0hd.xn--p1ai/api/logist', { latitude, longitude, status }, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
